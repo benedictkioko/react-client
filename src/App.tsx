88e63a4ad46355ab6c5260a899ab9a6fa4b5d745
benedict <kiokobenedict@gmail.com>
2021-02-22 10:41:30 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/main.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="bold to-gray-900">Hello from SCOT</h1>
+    </div>
+  );
+}
+
+export default App;
